8f5f5d3a5bb6ee89f85e0164b6b61eb00950eed1
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-04-29 20:18:43 +0300

    add asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 module.exports = {
-
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
     module: {
         rules: [
             {
@@ -12,11 +16,13 @@ module.exports = {
                 "sass-loader",
                 ],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: './images/[name][ext]',
+                },
+            },
         ],
-    },
-    entry: './src/index.js',
-    output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
     },
 };
